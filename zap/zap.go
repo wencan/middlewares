5e5de0a62ec9c5ced9f1f610dc63007f838969e9
wencan <i@wencan.org>
2019-07-04 13:35:29 +0800

    add zapLogger

---
+++ zap/zap.go
@@ -0,0 +1,65 @@
+package zap
+
+import (
+	"net/http"
+	"time"
+
+	"go.uber.org/zap"
+)
+
+// Logger Implement of LoggingLogger
+type Logger struct {
+	logger *zap.Logger
+}
+
+// NewLogger create ZapLogger object
+func NewLogger(logger *zap.Logger) *Logger {
+	return &Logger{
+		logger: logger,
+	}
+}
+
+func buildZapFields(req *http.Request, status, bodyBytesSent int, timestamp time.Time) []zap.Field {
+	fields := []zap.Field{}
+	fields = append(fields, zap.String("remote_addr", req.RemoteAddr))
+	user, _, ok := req.BasicAuth()
+	if ok {
+		fields = append(fields, zap.String("remote_user", user))
+	}
+	fields = append(fields, zap.Time("time_local", timestamp))
+	fields = append(fields, zap.String("request_method", req.Method))
+	fields = append(fields, zap.String("request_uri", req.RequestURI))
+	fields = append(fields, zap.String("server_protocol", req.Proto))
+	fields = append(fields, zap.Int("status", status))
+	fields = append(fields, zap.Int("body_bytes_sent", bodyBytesSent))
+	if req.Referer() != "" {
+		fields = append(fields, zap.String("http_referer", req.Referer()))
+	}
+	if req.UserAgent() != "" {
+		fields = append(fields, zap.String("http_user_agent", req.UserAgent()))
+	}
+	fields = append(fields, zap.String("elapsed_time", time.Now().Sub(timestamp).String()))
+	return fields
+}
+
+// Write logs a message.
+func (logging *Logger) Write(req *http.Request, status, bodyBytesSent int, timestamp time.Time) error {
+	fields := buildZapFields(req, status, bodyBytesSent, timestamp)
+
+	statusText := http.StatusText(status)
+	switch status / 100 {
+	case 1:
+		logging.logger.Info(statusText, fields...)
+	case 2:
+		logging.logger.Info(statusText, fields...)
+	case 3:
+		logging.logger.Info(statusText, fields...)
+	case 4:
+		logging.logger.Warn(statusText, fields...)
+	case 5:
+		logging.logger.Error(statusText, fields...)
+	default:
+		logging.logger.Warn("Unknown HTTP status", fields...)
+	}
+	return nil
+}
